beff3c2d32d52c482fb3f9b08a1714dd1365af2d
chroniicallydiistracted <chroniicallydiistracted@gmail.com>
2025-08-31 22:38:43 -0700

    Add React/Node TS types and next-env for Vercel build

---
+++ apps/web/next-env.d.ts
@@ -0,0 +1,6 @@
+/// <reference types="next" />
+/// <reference types="next/image-types/global" />
+/*
+  NOTE: This file should not be edited.
+  It ensures Next.js type definitions are available to TypeScript.
+*/
