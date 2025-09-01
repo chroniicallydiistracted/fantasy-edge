c72df750282fd22d884b17d1591089a8652dca1b
chroniicallydiistracted <chroniicallydiistracted@gmail.com>
2025-08-31 22:58:32 -0700

    delete: remove unused Callback component and associated metadata

--- apps/web/app/(auth)/callback/page.tsx
+++
@@ -1,6 +0,0 @@
-'use client'
-import { useSearchParams } from 'next/navigation'
-export default function Callback() {
-  const sp = useSearchParams();
-  return <pre>{JSON.stringify({ code: sp.get('code'), state: sp.get('state') }, null, 2)}</pre>
-}
