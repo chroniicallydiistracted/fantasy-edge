5bd3fa18999e228e939f54e6d812f758642ee1f2
CHRONiiCALLY DiiSTRACTED <chroniicallydiistracted@gmail.com>
2025-09-02 12:23:25 -0700

    chore: refactor env.py to use environment variable for database URL and remove deprecated env_new.py

--- apps/api/alembic/env.py
+++ apps/api/alembic/env.py
@@ -1,9 +1,15 @@
+import os
 from logging.config import fileConfig
-
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
-
 from alembic import context
+
+
+config = context.config
+db_url = os.getenv("DATABASE_URL")
+if db_url:
+    config.set_main_option("sqlalchemy.url", db_url)
+
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
